1c35d381f7b885c538e5fd367b75c86b37e30f88
Muhammad Nabeel Arif <nabeel.arif100@gmail.com>
2016-04-25 23:19:11 +0500

    Initial Commit

---
+++ FyberOffers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FyberOffers
+//
+//  Created by Nabeel Arif on 4/25/16.
+//  Copyright © 2016 Nabeel Arif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
